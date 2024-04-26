972fa325ce3e413062ce7b7b4ba86344ea600cf8
Tom Grossi <TheAutomaTom@gmail.com>
2024-04-25 20:45:24 -0500

    Organize test files

---
+++ E2E.Tests.Integration/Api_to_Testcontainer_Tests/CustomerWebApplicationFactory.cs
@@ -0,0 +1,30 @@
+﻿using E2E.Api;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+
+namespace E2E.Tests.Integration.Api_to_Testcontainer_Tests
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+
+        public CustomWebApplicationFactory()
+        {
+
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            base.ConfigureWebHost(builder);
+
+            builder.ConfigureTestServices(services =>
+            {
+                // Override typical behavior here...
+                Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Test");
+
+            });
+        }
+
+
+    }
+}
